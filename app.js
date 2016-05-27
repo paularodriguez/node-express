f16a27e01fa11ef2fe1ddddf38c00ee3118915ae
Paula Rodriguez <paula.rp93@gmail.com>
2016-05-27 10:58:16 +0200

    Add initial test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
+	//throw 'Error';
 	response.send('Ok');
 });
 
-app.listen(3000);
+module.exports = app;
+//app.listen(3000);
